c7f6a83c3530db80ac22a8ad53f2b30eb51817a1
shivamkarn <shivamkarn767@gmail.com>
2026-02-23 21:54:01 +0545

    Initialize question

---
+++ Self_Ques/AddUpperLowerNibble.asm
@@ -0,0 +1,2 @@
+;Write a program for 8085 to add upper nibble and lower nibble of ten 8-bit words stored in a table that starts from location 8B20H.
+;Store the separate results in location just after the table.
