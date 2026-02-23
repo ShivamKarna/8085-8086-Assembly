8d68d93242504f66874500ceed501f4bbccf0570
shivamkarn <shivamkarn767@gmail.com>
2026-02-23 21:29:09 +0545

    Add assembly program to count ones in upper nibble of ten 8-bit numbers

---
+++ Self_Ques/CountOneInUpperNibble.asm
@@ -0,0 +1,26 @@
+; Write a program for 8085 to calculate the numbers of ones in the upper nibble of ten 8-bit numbers stored in table. 
+ ;Store the count in memory location just after the table. 	(8) 	 	 	[2072  Ashwin] 
+
+ LXI H, 2000H 
+ MVI C, 14H 
+ MVI D, 00H 
+
+ LOOP : MOV A,M 
+          ANI F0H 
+
+          MVI B, 04H 
+
+CHECK_BIT : RLC 
+                    JNC SKIP_INC
+                    INR D 
+            
+SKIP_INC : DCR B 
+                  JNZ CHECK_BIT 
+
+
+                  INX H 
+                  DCR C 
+                  JNZ LOOP 
+
+                  MOV M , D 
+HLT
